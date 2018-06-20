f0557979e36e90f8f531f19de178ec6eb06725c8
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-06-20 11:56:25 +0200

    Exercice 5 JS

---
+++ assets/js/cours.js
@@ -0,0 +1,5 @@
+function Number(){
+  var number1= document.getElementById("firstNumber").value;
+  var number2= document.getElementById("secondNumber").value;
+  alert(parseInt(number1) *number2);
+}
