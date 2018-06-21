dcdbe08053b194f9ae035b97f51704108cbcc469
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-06-21 10:00:23 +0200

    correction avec ajout de la protection de saisie

--- assets/js/cours.js
+++ assets/js/cours.js
@@ -1,5 +1,10 @@
 function Number(){
   var number1= document.getElementById("firstNumber").value;
   var number2= document.getElementById("secondNumber").value;
-  alert(parseInt(number1) *number2);
+  var regex = /^[0-9]+[.1][0-9]+$/;
+  if((regex.test(number1) == true) && (regex.test(number2) == true)){
+    alert(parseInt(number1)*number2);
+  }else{
+    alert('N\'intégrez que des chiffres à virgule dans votre saisie');
+  }
 }
